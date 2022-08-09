89de3cb18a1d508a7b92d8aefc1c51762d41832f
Per Odlund <per.odlund@gmail.com>
2022-08-10 01:05:42 +0200

    A transmission script that kinda works if the torrent is well behaved and the series/tmdb does too

--- transmission.sh
+++ transmission.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
-# This doesn't let you seed, a "later" problem.
+# This doesn't let you seed because it might create a directory
 # Maybe rewrite this in python or something to make that work
 
 HOST=localhost
@@ -11,18 +11,21 @@ PORT=9091
 # REMOTE="transmission-remote $HOST:$PORT --auth=$USER:$PASS"
 REMOTE="transmission-remote $HOST:$PORT"
 
-FILES=$($REMOTE -t $TR_TORRENT_ID -S -f | tail -n +4 | awk -F ' ' '{print $7}')
-ROOTFILES=$(echo $FILES | perl -pe 's|(.*?/).*|\1|')
-ROOT=$(echo $FILES | uniq)
-NEWDIR=$(echo $ROOT | wc -l)
+# remove -S
+FILES=$($REMOTE -t $TR_TORRENT_ID -f | tail -n +4 | awk -F ' ' '{print $7}')
+ROOTFILES=$(for file in $(echo $FILES); do echo $file; done | perl -pe 's|(.*?/).*|\1|' | uniq)
+NEWDIR=$(echo $ROOTFILES | wc -l)
 
-if [ $NEWDIR -gt 1 ] then
-	DIR=$TR_TORRENT_DIR/$TR_TORRENT_NAME
+if [ $NEWDIR -gt 1 ]; then
+	DIR=${TR_TORRENT_DIR}/${TR_TORRENT_NAME}
 	mkdir $DIR
 	$REMOTE -t $TR_TORRENT_ID --move $DIR
 else
-	DIR=$TR_TORRENT_DIR/$ROOT
+	# maybe like this
+	DIR=$TR_TORRENT_DIR/${ROOTFILES}
 fi
 
-movix add $DIR
-$REMOTE -t $TR_TORRENT_ID -rad
+# echo movix add "$DIR"
+/home/dagle/code/movix/movix add "$DIR" 2>&1 >> /home/dagle/transmission.log
+echo $? >> /home/dagle/transmission.log
+# $REMOTE -t $TR_TORRENT_ID -rad
